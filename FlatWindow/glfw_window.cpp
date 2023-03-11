ebb5a6803e1616965f7160c382f51a6d5216d0b4
IAKSH <1248675212@qq.com>
2023-03-11 23:17:13 +0800

    fix glfwInitHint

--- FlatWindow/glfw_window.cpp
+++ FlatWindow/glfw_window.cpp
@@ -18,8 +18,8 @@ void flat::FWWindow::initGLAD()
 void flat::FWWindow::initGLFW()
 {
     glfwInit();
-    glfwInitHint(GLFW_VERSION_MAJOR, 3);
-    glfwInitHint(GLFW_VERSION_MINOR, 3);
+    glfwInitHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwInitHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwInitHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
