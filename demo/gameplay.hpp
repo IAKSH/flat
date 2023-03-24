f695488325084048e125eaf36e7dc2757f8dff2d
IAKSH <1248675212@qq.com>
2023-03-24 17:33:05 +0800

    collision check (gjk)

--- demo/gameplay.hpp
+++ demo/gameplay.hpp
@@ -5,6 +5,7 @@
 #include <initializer_list>
 #include <memory>
 #include <vector>
+#include <array>
 #include <iterator>
 #include <algorithm>
 #include <string_view>
@@ -18,6 +19,38 @@ namespace flat
     {
     private:
         float posX, posY, width, height, rotate;
+
+        struct Vec2
+        {
+            float x, y;
+            Vec2()
+                : x(0.0f), y(0.0f)
+            {}
+
+            Vec2(float _x, float _y)
+                : x(_x), y(_y)
+            {}
+        };
+
+        Vec2 subtract(Vec2 a, Vec2 b);
+        Vec2 negate(Vec2 v);
+        Vec2 perpendicular(Vec2 v);
+        float dotProduct(Vec2 a, Vec2 b);
+        float lengthSquared(Vec2 v);
+
+        Vec2 tripleProduct(Vec2 a, Vec2 b, Vec2 c);
+        Vec2 averagePoint(const Vec2* vertices, size_t count);
+        size_t indexOfFurthestPoint(const Vec2* vertices, size_t count, Vec2 d);
+        Vec2 support(const Vec2* vertices1, size_t count1,const Vec2* vertices2, size_t count2, Vec2 d);
+
+        int iter_count = 0;
+
+        int gjk(const Vec2* vertices1, size_t count1, const Vec2* vertices2, size_t count2);
+
+        float Perturbation();
+        Vec2 Jostle(Vec2 a);
+
+        std::array<Vec2, 4> getHitboxVertexCoords(Physics2D& obj);
 
     public:
         Physics2D();
