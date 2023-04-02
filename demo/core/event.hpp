64e55b2b4312470c7001a98cbf3b0c37afa668b5
IAKSH <1248675212@qq.com>
2023-04-02 19:13:11 +0800

    remove class EventDispatcher

--- demo/core/event.hpp
+++ demo/core/event.hpp
@@ -12,29 +12,4 @@ namespace flat::core
 		bool getHandled() { return handled; }
 		void setHandled(bool b) { handled = b; }
 	};
-
-	class EventDispatcher
-	{
-	private:
-		Event& event;
-
-	public:
-		EventDispatcher(Event& e)
-			: event(e)
-		{
-		}
-
-		// F will be deduced by the compiler
-		template<typename T, typename F>
-		bool Dispatch(const F& func)
-		{
-			// RTTI
-			if (typeid(event) == typeid(F))
-			{
-				event.setHandled(func(static_cast<T&>(event)));
-				return true;
-			}
-			return false;
-		}
-	};
 }
