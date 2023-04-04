7339ae4dc5a5478663632cc7a09ad32c059d786e
IAKSH <1248675212@qq.com>
2023-04-04 23:01:13 +0800

    class Camera2D

--- demo/ni/camera.cpp
+++ demo/ni/camera.cpp
@@ -1,37 +1,82 @@
 #include "camera.hpp"
+#include "GLFW/glfw3.h"
 
 flat::ni::Camera::Camera()
-	: fov{ 60.0f }, aspectRatio{ 16.0f / 9.0f }, nearPlane{ 0.01f }, farPlane{ 100.0f },
-	front{ 1.0f,0.0f,0.0f }, up{ 0.0f,0.0f,1.0f }
+	: fov{ 45.0f }, aspectRatio{ 16.0f / 9.0f }, nearPlane{ 0.01f }, farPlane{ 100.0f }, yaw(0.0f), pitch(0.0f), zoom(0.0f),
+	up{ 0.0f, 0.0f, 1.0f }, worldUp{ up }
 {
+	update();
 }
 
 glm::mat4 flat::ni::Camera::getTranslateMatrix()
 {
-	glm::vec3 position{ getPositionX(), getPositionY(), getPositionZ() };
-	return glm::perspective(glm::radians(fov), aspectRatio, nearPlane, farPlane) *
-		glm::lookAt(position, position + front, up);
+	glm::vec3 position{ getPositionX(),getPositionY(),getPositionZ() };
+	auto win = glfwGetCurrentContext();
+	int h, w;
+	glfwGetWindowSize(win, &w, &h);
+	glm::mat4 projection = glm::perspective(glm::radians(fov + zoom), (float)w / (float)h, nearPlane, farPlane);
+	return projection * glm::lookAt(position, position + front, up);
 }
 
-void flat::ni::Camera::move(float forward, float right)
+void flat::ni::Camera::move(float dforward, float dright)
 {
 	glm::vec3 position{ getPositionX(),getPositionY(),getPositionZ() };
-	position += forward * front;
-	position += right * glm::normalize(glm::cross(front, up));
+	position += front * dforward;
+	position += right * dright;
 	setPositionX(position[0]);
 	setPositionY(position[1]);
 	setPositionZ(position[2]);
 }
-
-void flat::ni::Camera::rotate(float rup,float rright)
+#include <iostream>
+void flat::ni::Camera::turn(float upOffset, float rightOffset)
 {
-	glm::vec3 position{ getPositionX(),getPositionY(),getPositionZ() };
+	// Calculate the new front vector based on the current yaw and pitch
+	glm::vec3 newFront;
+	newFront.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	newFront.y = sin(glm::radians(pitch));
+	newFront.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
 
-	glm::mat4 rotateRight = glm::rotate(glm::mat4(1.0f), glm::radians(rright), up);
-	front = glm::vec3(rotateRight * glm::vec4(front, 1.0f));
+	// Calculate the right and up vectors based on the current front vector and world up vector
+	glm::vec3 newRight = glm::normalize(glm::cross(newFront, worldUp));
+	glm::vec3 newUp = glm::normalize(glm::cross(newRight, newFront));
 
-	glm::vec3 rightAxis = glm::cross(front, up);
-	glm::mat4 rotateUp = glm::rotate(glm::mat4(1.0f), glm::radians(rup), rightAxis);
-	front = glm::vec3(rotateUp * glm::vec4(front, 1.0f));
-	up = glm::vec3(rotateUp * glm::vec4(up, 1.0f));
+	// Rotate the up and right vectors by the specified offsets
+	glm::mat4 rotation = glm::rotate(glm::mat4(1.0f), glm::radians(upOffset), newRight);
+	rotation = glm::rotate(rotation, glm::radians(rightOffset), newUp);
+	up = glm::vec3(rotation * glm::vec4(up, 1.0f));
+	right = glm::normalize(glm::cross(front, up)); // Recalculate right vector
+
+	// Update the pitch and yaw values based on the new front vector
+	pitch = glm::degrees(asin(newFront.y));
+	yaw = glm::degrees(atan2(newFront.z, newFront.x));
+
+	// Update the front vector based on the new pitch and yaw values
+	newFront.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	newFront.y = sin(glm::radians(pitch));
+	newFront.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+	front = glm::normalize(newFront);
+}
+
+void flat::ni::Camera::update()
+{
+	// calculate new front vector using the updated Euler angles
+	glm::vec3 newFront;
+	newFront.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	newFront.y = sin(glm::radians(pitch));
+	newFront.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+
+	// update front, right and up vectors
+	front = glm::normalize(newFront);
+	right = glm::normalize(glm::cross(front, worldUp));
+	up = glm::normalize(glm::cross(right, front));
+}
+
+glm::mat4 flat::ni::Camera2D::getTranslateMatrix()
+{
+	GLFWwindow* win = glfwGetCurrentContext();
+	int w, h;
+	glfwGetWindowSize(win, &w, &h);
+	glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(w), 0.0f, static_cast<float>(h), -1.0f, 1.0f);
+	glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-getPositionX() + w / 2, -getPositionY() + h / 2, 0.0f));
+	return projection * view;
 }
--- demo/ni/camera.hpp
+++ demo/ni/camera.hpp
@@ -8,6 +8,15 @@
 
 namespace flat::ni
 {
+	class Camera2D : public flat::ni::MassPoint
+	{
+	public:
+		Camera2D() = default;
+		~Camera2D() = default;
+		glm::mat4 getTranslateMatrix();
+	};
+
+	// abandoned
 	class Camera : public flat::ni::MassPoint
 	{
 	private:
@@ -15,8 +24,14 @@ namespace flat::ni
 		float aspectRatio;// 宽高比
 		float nearPlane;// 近裁剪距离
 		float farPlane;// 远裁剪距离
-		glm::vec3 front;// 相机的方向向量
-		glm::vec3 up;// 相机竖直方向的方向向量
+		float yaw;
+		float pitch;
+		//float roll;
+		float zoom;
+		glm::vec3 front;
+		glm::vec3 up;
+		glm::vec3 right;
+		glm::vec3 worldUp;
 
 	public:
 		Camera();
@@ -25,23 +40,17 @@ namespace flat::ni
 		const float& getFov() { return fov; }
 		const float& getNearPlane() { return nearPlane; }
 		const float& getFarPlane() { return farPlane; }
-		const float& getFrontX() { return front[0]; }
-		const float& getFrontY() { return front[1]; }
-		const float& getFrontZ() { return front[2]; }
-		const float& getUpX() { return up[0]; }
-		const float& getUpY() { return up[1]; }
-		const float& getUpZ() { return up[2]; }
+		const float& getYaw() { return yaw; }
+		const float& getPitch() { return pitch; }
 		void setFov(const float& val) { fov = val; }
 		void setNearPlane(const float& val) { nearPlane = val; }
 		void setFarPlane(const float& val) { farPlane = val; }
-		void setFrontX(const float& val) { front[0] = val; }
-		void setFrontY(const float& val) { front[1] = val; }
-		void setFrontZ(const float& val) { front[2] = val; }
-		void setUpX(const float& val) { up[0] = val; }
-		void setUpY(const float& val) { up[1] = val; }
-		void setUpZ(const float& val) { up[2] = val; }
+		void setYaw(const float& val) { yaw = val; }
+		void setPitch(const float& val) { pitch = val; }
+		void setZoom(const float& val) { zoom = val; }
 
-		void move(float forward,float right);
-		void rotate(float rup,float rright);
+		void move(float dForward, float dRight);
+		void turn(float dPitch, float dYaw);
+		void update();
 	};
 }
