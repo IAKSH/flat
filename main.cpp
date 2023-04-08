dc0726e2e70618218a3af949c6b7f40786cf684b
IAKSH <1248675212@qq.com>
2023-04-08 10:48:49 +0800

    animation test

--- main.cpp
+++ main.cpp
@@ -57,18 +57,28 @@ class MainLayer : public ni::core::Layer
 {
 private:
 	ni::utils::Shader mainShader;
-	ni::utils::TimeRecorder recoreder;
+	ni::utils::TimeRecorder recorder;
+	ni::utils::TimeRecorder birdAniRecorder;
 	ni::utils::Timer timer;
 	ni::utils::Texture testTex;
-	ni::utils::Texture birdTex;
+	std::array<ni::utils::Texture,3> birdTexs;
+	std::array<ni::utils::Texture,3>::iterator currentBirdTex { birdTexs.begin() };
+	ni::utils::Texture backgroundTex;
 	ni::utils::Audio testAudio;
 	ni::utils::Camera2D cam;
 	ni::utils::VertexArrayObj vao;
+	ni::utils::VertexArrayObj backgroundVAO;
+	ni::utils::VertexArrayObj birdVAO;
 	ni::utils::AudioSource testAudioSource;
 
 	float camDownVec{ 0.0f };
 	float camLeftVec{ 0.0f };
 	float camSensitivity{ 2.5f };
+
+	float birdPosX { 0.0f };
+	float birdPosY { 0.0f };
+	float birdVelX { 0.0f };
+	float birdVelY { 0.0f };
 
 public:
 	MainLayer()
@@ -88,10 +98,16 @@ public:
 
 		// Set up vertex data and buffers
 		std::array<float,36> vertices {
-				100.0f,  100.0f,  0.0f, 1.0f, 1.0f, 1.0f, 0.1f, 1.0f, 1.0f,  // top right
-				100.0f,  -100.0f, 0.0f,1.0f, 1.0f, 1.0f, 0.1f, 1.0f, 0.0f,  // bottom right
-				-100.0f, -100.0f, 0.0f,1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,  // bottom left
-				-100.0f, 100.0f,  0.0f,1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f   // top left
+				1.0f,  1.0f,  0.0f, 1.0f, 1.0f, 1.0f, 0.1f, 1.0f, 1.0f,  // top right
+				1.0f,  -1.0f, 0.0f,1.0f, 1.0f, 1.0f, 0.1f, 1.0f, 0.0f,  // bottom right
+				-1.0f, -1.0f, 0.0f,1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,  // bottom left
+				-1.0f, 1.0f,  0.0f,1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f   // top left
+		};
+		std::array<float,36> nonTransparentVertices {
+				1.0f,  1.0f,  0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,  // top right
+				1.0f,  -1.0f, 0.0f,1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,  // bottom right
+				-1.0f, -1.0f, 0.0f,1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,  // bottom left
+				-1.0f, 1.0f,  0.0f,1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f   // top left
 		};
 		std::array<unsigned int, 6> indices {
 			0, 1, 3,  // first Triangle
@@ -99,6 +115,8 @@ public:
 		};
 
 		vao.create(ni::utils::GLBufferType::Static,vertices,indices);
+		backgroundVAO.create(ni::utils::GLBufferType::Static,nonTransparentVertices,indices);
+		birdVAO.create(ni::utils::GLBufferType::Static,nonTransparentVertices,indices);
 
 		// imgui test
 		IMGUI_CHECKVERSION();
@@ -110,7 +128,10 @@ public:
 
 		// texture test
 		testTex.loadFromFile("images/floor.png");
-		birdTex.loadFromFile("images/bird0_0.png");
+		birdTexs[0].loadFromFile("images/bird0_0.png");
+		birdTexs[1].loadFromFile("images/bird0_1.png");
+		birdTexs[2].loadFromFile("images/bird0_2.png");
+		backgroundTex.loadFromFile("images/strangeSky.png");
 
 		// audio test
 		testAudio.loadFromFile("sounds/demo_sounds_relaxed-vlog-night-street-131746.mp3");
@@ -123,8 +144,8 @@ public:
 		timer.setInterval(ni::utils::MilliSeconds(3));
 		timer.detachRun([&]() 
 		{
-			if(ni::flat::collisionCheckGJK(cam.getPositionX(),cam.getPositionY(),0.1f,0.1f,0.0f,0.0f,0.0f,100.0f,100.0f,glfwGetTime()))
-				ni::utils::otherLogger()->warn("collision: cam at ({},{}) hit obj at ({},{})",cam.getPositionX(),cam.getPositionY(),0.0f,0.0f);
+			if(ni::flat::collisionCheckGJK(birdPosX,birdPosY,50.0f,50.0f,0.0f,0.0f,0.0f,100.0f,100.0f,glfwGetTime()))
+				ni::utils::otherLogger()->warn("collision: bird at ({},{}) hit obj at ({},{})",birdPosX,birdPosY,0.0f,0.0f);
 		});
 	}
 
@@ -142,34 +163,86 @@ public:
 		//cam.update();
 		cam.setPositionX(cam.getPositionX() + cam.getVelocityX() * 2);
 		cam.setPositionY(cam.getPositionY() + cam.getVelocityY() * 2);
+
+		birdPosX += birdVelX;
+		birdPosY += birdVelY;
+
+		if(birdAniRecorder.getSpanAsMilliSeconds() >= ni::utils::MilliSeconds(250))
+		{
+			if(++currentBirdTex;currentBirdTex == birdTexs.end())
+				currentBirdTex = birdTexs.begin();
+
+			birdAniRecorder.update();
+		}
 	}
 
 	virtual void onRender() override
 	{
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glEnable(GL_DEPTH_TEST);
+		//glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
 		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, testTex.getTextureID());
 
-		glm::mat4 trans(1.0f);
-		trans *= glm::translate(glm::mat4(1.0f), glm::vec3(sin(glfwGetTime()) / 2.0f, cos(glfwGetTime()) / 2.0f, 0.0f));
-		trans *= glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 1.0f, 1.0f));
-		trans *= glm::rotate(glm::mat4(1.0f), static_cast<float>(sin(glfwGetTime())), glm::vec3(0.0f, 0.0f, 1.0f));
+		// draw background
+		{
+			glBindTexture(GL_TEXTURE_2D, backgroundTex.getTextureID());
 
-		unsigned int transLocation = glGetUniformLocation(mainShader.getShaderID(), "transform");
-		glUniformMatrix4fv(transLocation, 1, GL_FALSE, glm::value_ptr(trans));
+			glm::mat4 trans(1.0f);
+			trans *= glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+			trans *= glm::scale(glm::mat4(1.0f), glm::vec3(800.0f, 600.0f, 1.0f));
+			trans *= glm::rotate(glm::mat4(1.0f), 0.0f, glm::vec3(0.0f, 0.0f, 1.0f));
 
-		unsigned int camTrans = glGetUniformLocation(mainShader.getShaderID(), "camTrans");
-		glUniformMatrix4fv(camTrans, 1, GL_FALSE, glm::value_ptr(cam.getTranslateMatrix()));
+			unsigned int transLocation = glGetUniformLocation(mainShader.getShaderID(), "transform");
+			glUniformMatrix4fv(transLocation, 1, GL_FALSE, glm::value_ptr(trans));
+			unsigned int camTrans = glGetUniformLocation(mainShader.getShaderID(), "camTrans");
+			glUniformMatrix4fv(camTrans, 1, GL_FALSE, glm::value_ptr(cam.getTranslateMatrix()));
 
-		glBindVertexArray(vao.getVAO());
-		glUseProgram(mainShader.getShaderID());
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		glBindVertexArray(0);
+			glBindVertexArray(backgroundVAO.getVAO());
+			glUseProgram(mainShader.getShaderID());
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+			glBindVertexArray(0);
+		}
+		// draw floor
+		{
+			glBindTexture(GL_TEXTURE_2D, testTex.getTextureID());
+
+			glm::mat4 trans(1.0f);
+			trans *= glm::translate(glm::mat4(1.0f), glm::vec3(sin(glfwGetTime()) / 2.0f, cos(glfwGetTime()) / 2.0f, 0.1f));
+			trans *= glm::scale(glm::mat4(1.0f), glm::vec3(100.0f, 100.0f, 1.0f));
+			trans *= glm::rotate(glm::mat4(1.0f), static_cast<float>(sin(glfwGetTime())), glm::vec3(0.0f, 0.0f, 1.0f));
+
+			unsigned int transLocation = glGetUniformLocation(mainShader.getShaderID(), "transform");
+			glUniformMatrix4fv(transLocation, 1, GL_FALSE, glm::value_ptr(trans));
+			unsigned int camTrans = glGetUniformLocation(mainShader.getShaderID(), "camTrans");
+			glUniformMatrix4fv(camTrans, 1, GL_FALSE, glm::value_ptr(cam.getTranslateMatrix()));
+
+			glBindVertexArray(vao.getVAO());
+			glUseProgram(mainShader.getShaderID());
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+			glBindVertexArray(0);
+		}
+		// draw bird
+		{
+			glBindTexture(GL_TEXTURE_2D, currentBirdTex->getTextureID());
+
+			glm::mat4 trans(1.0f);
+			trans *= glm::translate(glm::mat4(1.0f), glm::vec3(birdPosX,birdPosY,0.2f));
+			trans *= glm::scale(glm::mat4(1.0f), glm::vec3(50.0f, 50.0f, 1.0f));
+			trans *= glm::rotate(glm::mat4(1.0f), 0.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+
+			unsigned int transLocation = glGetUniformLocation(mainShader.getShaderID(), "transform");
+			glUniformMatrix4fv(transLocation, 1, GL_FALSE, glm::value_ptr(trans));
+			unsigned int camTrans = glGetUniformLocation(mainShader.getShaderID(), "camTrans");
+			glUniformMatrix4fv(camTrans, 1, GL_FALSE, glm::value_ptr(cam.getTranslateMatrix()));
+
+			glBindVertexArray(birdVAO.getVAO());
+			glUseProgram(mainShader.getShaderID());
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+			glBindVertexArray(0);
+		}
 
 		// imgui test
 		ImGui_ImplOpenGL3_NewFrame();
@@ -178,7 +251,7 @@ public:
 
 		ImGui::Begin("ImGui Test");
 		ImGui::Text("glfwGetTime(): %lf", glfwGetTime());
-		ImGui::Text("FPS: %f", 1000000.0f / recoreder.getSpanAsMicroSeconds().count());
+		ImGui::Text("FPS: %f", 1000000.0f / recorder.getSpanAsMicroSeconds().count());
 		ImGui::Text("cam.x: %f", cam.getPositionX());
 		ImGui::Text("cam.y: %f", cam.getPositionY());
 		ImGui::Text("cam.z: %f", cam.getPositionZ());
@@ -189,7 +262,7 @@ public:
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-		recoreder.update();
+		recorder.update();
 	}
 
 	virtual void onEvent(ni::core::Event& e) override
@@ -198,6 +271,18 @@ public:
 		{
 			switch (static_cast<ni::core::KeyPressEvent&>(e).getKeyCode())
 			{
+				case ni::core::KeyCode::UP:
+					birdVelY = 0.5f;
+					break;
+				case ni::core::KeyCode::DOWN:
+					birdVelY = -0.5f;
+					break;
+				case ni::core::KeyCode::RIGHT:
+					birdVelX = 0.5f;
+					break;
+				case ni::core::KeyCode::LEFT:
+					birdVelX = -0.5f;
+					break;
 				case ni::core::KeyCode::W:
 					cam.setVelocityY(0.1f);
 					break;
@@ -210,6 +295,7 @@ public:
 				case ni::core::KeyCode::A:
 					cam.setVelocityX(-0.1f);
 					break;
+				/*
 				case ni::core::KeyCode::Q:
 					camDownVec = 1.0f;
 					break;
@@ -222,6 +308,7 @@ public:
 				case ni::core::KeyCode::LEFT_CONTROL:
 					camLeftVec = -1.0f;
 					break;
+				*/
 				case ni::core::KeyCode::ESCAPE:
 					ni::core::Application::getInstance()->exit();
 					break;
@@ -233,6 +320,14 @@ public:
 		{
 			switch (static_cast<ni::core::KeyReleaseEvent&>(e).getKeyCode())
 			{
+				case ni::core::KeyCode::LEFT:
+				case ni::core::KeyCode::RIGHT:
+					birdVelX = 0.0f;
+					break;
+				case ni::core::KeyCode::UP:
+				case ni::core::KeyCode::DOWN:
+					birdVelY = 0.0f;
+					break;
 				case ni::core::KeyCode::W:
 				case ni::core::KeyCode::S:
 					cam.setVelocityY(0.0f);
