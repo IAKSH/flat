af2801b7e9f7715759f1a26879a90eb79ceca09f
IAKSH <1248675212@qq.com>
2023-04-21 22:30:38 +0800

    moved logger from utils/ to core/

---
+++ core/logger.cpp
@@ -0,0 +1,24 @@
+#include "logger.hpp"
+#include <exception>
+#include <iostream>
+
+ni::utils::Logger::Logger(std::string_view name)
+    : logrName{name}
+{
+    initialize();
+}
+
+void ni::utils::Logger::initialize()
+{
+    try
+    {
+        spdlog::set_pattern("%^[%T] %n: %v%$");
+        logger = spdlog::stdout_color_mt(logrName);
+        logger->set_level(spdlog::level::trace);
+    }
+    catch (const spdlog::spdlog_ex& ex)
+    {
+        std::cerr << "Logger initialization failed: " << ex.what() << std::endl;
+        std::terminate();
+    }
+}
---
+++ core/logger.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "template.hpp"
+#include <spdlog/spdlog.h>
+#include <spdlog/fmt/ostr.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <memory>
+#include <string>
+
+namespace ni::utils
+{
+    class Logger : public DisableCopy
+    {
+    private:
+        std::string logrName;
+        std::shared_ptr<spdlog::logger> logger;
+        void initialize();
+
+    public:
+        Logger(std::string_view name);
+        ~Logger() = default;
+        const std::shared_ptr<spdlog::logger>& operator->() {return logger;}
+    };
+}
