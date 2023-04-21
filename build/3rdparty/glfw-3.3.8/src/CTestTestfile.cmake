8b4617c32a5b973945ab0df43aca78198fa5087a
ZRJ <59336306+IAKSH@users.noreply.github.com>
2023-04-21 20:29:14 +0800

    Delete build directory

--- build/3rdparty/glfw-3.3.8/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Programming-Playground/flat2/3rdparty/glfw-3.3.8/src
-# Build directory: D:/Programming-Playground/flat2/build/3rdparty/glfw-3.3.8/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
