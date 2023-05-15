45d0f3729f0e705abc7d76ae217eb6b160a5ce0c
IAKSH <1248675212@qq.com>
2023-05-15 23:17:23 +0800

    layer

---
+++ Core/layer.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "event.hpp"
+#include "../../Misc/disable_copy.hpp"
+#include <string>
+
+namespace flat
+{
+    class Layer : misc::DisableCopy
+    {
+    private:
+        std::string layer_name;
+
+    public:
+        Layer(std::string_view name = "Unnamed Layer")
+            : layer_name(name)
+        {}
+
+        ~Layer() = default;
+
+        std::string_view get_name() {return layer_name;}
+        virtual void on_attach() = 0;
+        virtual void on_detach() = 0;
+        virtual void on_update() = 0;
+        virtual void on_render() = 0;
+        virtual void on_event(Event& e) = 0;
+    };
+}
