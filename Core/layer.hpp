67db93deb2e3494c2c439c48ac7fedd011bb7368
IAKSH <1248675212@qq.com>
2023-05-20 21:31:10 +0800

    changed layer's api to callback binding

--- Core/layer.hpp
+++ Core/layer.hpp
@@ -2,6 +2,7 @@
 
 #include "event.hpp"
 #include "../../misc/disable_copy.hpp"
+#include <functional>
 #include <string>
 
 namespace flat
@@ -10,19 +11,27 @@ namespace flat
     {
     private:
         std::string layer_name;
+        std::function<void()> callback_attach;
+        std::function<void()> callback_detach;
+        std::function<void()> callback_update;
+        std::function<void()> callback_render;
+        std::function<void(const Event& event)> callback_on_event;
 
     public:
-        Layer(std::string_view name = "Unnamed Layer")
-            : layer_name(name)
-        {}
-
+        Layer(std::string_view name) : layer_name(name) {}
+        Layer() : layer_name("Unnamed Layer") {}
         ~Layer() = default;
 
         std::string_view get_name() {return layer_name;}
-        virtual void on_attach() = 0;
-        virtual void on_detach() = 0;
-        virtual void on_update() = 0;
-        virtual void on_render() = 0;
-        virtual void on_event(const Event& event) = 0;
+        void on_attach() {if(callback_attach) callback_attach();}
+        void on_detach() {if(callback_detach) callback_detach();}
+        void on_update() {if(callback_update) callback_update();}
+        void on_render() {if(callback_render) callback_render();}
+        void on_event(const Event& event) {if(callback_on_event) callback_on_event(event);}
+        void set_attach_func(std::function<void()> func) {callback_attach = func;}
+        void set_detach_func(std::function<void()> func) {callback_detach = func;}
+        void set_update_func(std::function<void()> func) {callback_update = func;}
+        void set_render_func(std::function<void()> func) {callback_render = func;}
+        void set_event_func(std::function<void(const Event&)> func) {callback_on_event = func;}
     };
 }
--- tests/test.cpp
+++ tests/test.cpp
@@ -9,92 +9,102 @@ std::unique_ptr<flat::Texture> my_texture;
 std::unique_ptr<flat::Camera> camera;
 std::unique_ptr<flat::RenderPipe> pip;
 
-struct TestLayer : public flat::Layer
+//struct TestLayer : public flat::Layer
+//{
+//    TestLayer()
+//        : flat::Layer("Test Layer")
+//    {
+//    }
+//
+//    virtual void on_attach() override
+//    {
+//        std::cout << "hello!\n";
+//    }
+//
+//    virtual void on_detach() override
+//    {
+//        std::cout << "bye!\n";
+//    }
+//
+//    virtual void on_update() override
+//    {
+//
+//    }
+//
+//    virtual void on_render() override
+//    {
+//
+//    }
+//
+//    virtual void on_event(const flat::Event& event) override
+//    {
+//        if(event.get_type() == flat::EventType::KeyPress)
+//        {
+//            if(reinterpret_cast<const flat::KeyPressEvent*>(&event)->get_keycode() == flat::misc::KeyCode::ESCAPE)
+//                flat::Application::get_instance().exit();
+//            else if(reinterpret_cast<const flat::KeyPressEvent*>(&event)->get_keycode() == flat::misc::KeyCode::Z)
+//                my_texture->rotate(1.5f,0.5f,1.0f);
+//        }
+//    }
+//};
+//
+//struct TestDrawLayer : public flat::Layer
+//{
+//    TestDrawLayer()
+//        : flat::Layer("Test Draw Layer")
+//    {
+//    }
+//
+//    virtual void on_attach() override
+//    {
+//        std::cout << "test draw!\n";
+//        camera = std::make_unique<flat::Camera>(800,600);
+//
+//        my_image = std::make_unique<flat::Image>("E:\\Programming-Playground\\flat2\\red.png");
+//        my_texture = my_image->gen_texture(0,0,1024,576);
+//
+//        pip = std::make_unique<flat::RenderPipe>(flat::default_vertex_shader,flat::default_fragment_shader);
+//    }
+//
+//    virtual void on_detach() override
+//    {
+//        std::cout << "bye!\n";
+//    }
+//
+//    virtual void on_update() override
+//    {
+//        my_texture->set_position_x(my_texture->get_position_x() + sin(glfwGetTime()) * 0.0025f);
+//        my_texture->set_position_y(my_texture->get_position_y() + cos(glfwGetTime()) * 0.001f);
+//    }
+//
+//    virtual void on_render() override
+//    {
+//        glClearColor(0.1f, 0.1f, 0.1f, 0.5f);
+//        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+//        my_texture->flat::RenableObject::flush_to_screen();
+//        //pip->ren_texture(*my_texture)->flat::RenableObject::flush_to_screen();
+//    }
+//
+//    virtual void on_event(const flat::Event& event) override
+//    {
+//         std::cout << "event type: " << static_cast<int>(event.get_type()) << '\n';
+//    }
+//};
+
+int main()
 {
-    TestLayer()
-        : flat::Layer("Test Layer")
-    {
-    }
-
-    virtual void on_attach() override
-    {
-        std::cout << "hello!\n";
-    }
-
-    virtual void on_detach() override
-    {
-        std::cout << "bye!\n";
-    }
-
-    virtual void on_update() override
-    {
-
-    }
-
-    virtual void on_render() override
-    {
-
-    }
-
-    virtual void on_event(const flat::Event& event) override
+    auto my_test_layer {std::make_unique<flat::Layer>()};
+    my_test_layer->set_attach_func([](){std::cout << "hello world!\n";});
+    my_test_layer->set_detach_func([](){std::cout << "byebye!\n";});
+    my_test_layer->set_event_func([](const flat::Event& event)
     {
         if(event.get_type() == flat::EventType::KeyPress)
         {
             if(reinterpret_cast<const flat::KeyPressEvent*>(&event)->get_keycode() == flat::misc::KeyCode::ESCAPE)
                 flat::Application::get_instance().exit();
-            else if(reinterpret_cast<const flat::KeyPressEvent*>(&event)->get_keycode() == flat::misc::KeyCode::Z)
-                my_texture->rotate(1.5f,0.5f,1.0f);
         }
-    }
-};
+    });
 
-struct TestDrawLayer : public flat::Layer
-{
-    TestDrawLayer()
-        : flat::Layer("Test Draw Layer")
-    {
-    }
-
-    virtual void on_attach() override
-    {
-        std::cout << "test draw!\n";
-        camera = std::make_unique<flat::Camera>(800,600);
-
-        my_image = std::make_unique<flat::Image>("E:\\Programming-Playground\\flat2\\red.png");
-        my_texture = my_image->gen_texture(0,0,1024,576);
-
-        pip = std::make_unique<flat::RenderPipe>(flat::default_vertex_shader,flat::default_fragment_shader);
-    }
-
-    virtual void on_detach() override
-    {
-        std::cout << "bye!\n";
-    }
-
-    virtual void on_update() override
-    {
-        my_texture->set_position_x(my_texture->get_position_x() + sin(glfwGetTime()) * 0.0025f);
-        my_texture->set_position_y(my_texture->get_position_y() + cos(glfwGetTime()) * 0.001f);
-    }
-
-    virtual void on_render() override
-    {
-        glClearColor(0.1f, 0.1f, 0.1f, 0.5f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        my_texture->flat::RenableObject::flush_to_screen();
-        //pip->ren_texture(*my_texture)->flat::RenableObject::flush_to_screen();
-    }
-
-    virtual void on_event(const flat::Event& event) override
-    {
-         std::cout << "event type: " << static_cast<int>(event.get_type()) << '\n';
-    }
-};
-
-int main()
-{
-    std::cout << "hello world!\n";
-    flat::Application::get_instance().push_layer(std::make_unique<TestLayer>());
-    flat::Application::get_instance().push_layer(std::make_unique<TestDrawLayer>());
+    flat::Application::get_instance().push_layer(std::move(my_test_layer));
     flat::Application::get_instance().run();
 }
