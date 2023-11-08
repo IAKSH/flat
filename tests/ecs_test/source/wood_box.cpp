2f191dcb2c5406490938c4b6d96cc6de5cfff741
IAKSH <1248675212@qq.com>
2023-11-08 23:02:00 +0800

    change glad to OpenGL ES 3.2 + OpenGL 4.3 (core profile, debug), and remove wood_box from ecs_test

--- tests/ecs_test/source/wood_box.cpp
+++
@@ -1,8 +0,0 @@
-/*
-#include <ecs_test/wood_box.hpp>
-
-quick3d::test::WoodBoxRenderer::WoodBoxRenderer() noexcept(false)
-	: ssbo_model(GL_SHADER_STORAGE_BUFFER, GL_DYNAMIC_DRAW, sizeof(ModelData))
-{
-}
-*/
